0abf3b905df311017fb3b7b428cd78699866c17d
maxime.rattez <maxime.rattez@gmail.com>
2022-07-04 22:37:28 +0100

    first commit hihihihihihihihihihihihihi

--- config/navUser.php
+++ config/navUser.php
@@ -1,9 +1,9 @@
 <?php
-    if(!empty($_COOKIE)){
+    if(isset($_COOKIE['user_sujet_choice']) && isset($_COOKIE['user_radio_choice'])){
         $arrayThemeCookies = unserialize($_COOKIE['user_sujet_choice']);
         $sujet1 = $arrayThemeCookies[0];
         $sujet2 = $arrayThemeCookies[1];
         $sujet3 = $arrayThemeCookies[2];
         $radioCookie = $_COOKIE['user_radio_choice'];
 
-    }
+    } 
