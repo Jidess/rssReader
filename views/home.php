b8f0103eb86cad7a5bd2ac35470cd50efa2a666f
maxime.rattez <maxime.rattez@gmail.com>
2022-07-04 14:11:01 +0100

    first commit hihihihihihihihihihi

--- views/home.php
+++ views/home.php
@@ -2,37 +2,51 @@
     <div class="row">
         <div class="col-12 col-md-4">
         <?php
-            echo '<li>'.strip_tags($themes[$sujet1]->channel->item->description, '<img>').'</li>';
-            echo '<h4>'.$sujet1.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet1]->channel->item->pubDate);
+                $datetime = date_create($themes[$sujet1]->channel->item[$i]->pubDate);
                 $date = date_format($datetime, 'd M Y, H\hi');
-                echo '<li><a href="'.$themes[$sujet1]->channel->item->link.'">'.($themes[$sujet1]->channel->item->title).'</a> ('.$date.')</li>';
+                $description = htmlspecialchars_decode($themes[$sujet1]->channel->item[$i]->description);
+                preg_match('/src="([^"]+)"/', $description, $matches);
+                $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
+                $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
+                echo '<h4>'.$sujet1.'</h4>';
+                echo '<img src="'.$getImgClean.'">';
+                echo '<li><a href="'.$themes[$sujet1]->channel->item[$i]->link.'">'.($themes[$sujet1]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
             echo '</ul>';
         ?>
         </div>
         <div class="col-12 col-md-4">
         <?php
-            echo '<h4>'.$sujet2.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet2]->channel->item->pubDate);
+                $datetime = date_create($themes[$sujet2]->channel->item[$i]->pubDate);
                 $date = date_format($datetime, 'd M Y, H\hi');
-                echo '<li><a href="'.$themes[$sujet2]->channel->item->link.'">'.($themes[$sujet2]->channel->item->title).'</a> ('.$date.')</li>';
+                $description = htmlspecialchars_decode($themes[$sujet2]->channel->item[$i]->description);
+                preg_match('/src="([^"]+)"/', $description, $matches);
+                $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
+                $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
+                echo '<h4>'.$sujet2.'</h4>';
+                echo '<img src="'.$getImgClean.'">';
+                echo '<li><a href="'.$themes[$sujet2]->channel->item[$i]->link.'">'.($themes[$sujet2]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
             echo '</ul>';
         ?>
         </div>
         <div class="col-12 col-md-4">
         <?php
-            echo '<h4>'.$sujet3.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet3]->channel->item->pubDate);
+                $datetime = date_create($themes[$sujet3]->channel->item[$i]->pubDate);
                 $date = date_format($datetime, 'd M Y, H\hi');
-                echo '<li><a href="'.$themes[$sujet3]->channel->item->link.'">'.($themes[$sujet3]->channel->item->title).'</a> ('.$date.')</li>';
+                $description = htmlspecialchars_decode($themes[$sujet3]->channel->item[$i]->description);
+                preg_match('/src="([^"]+)"/', $description, $matches);
+                $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
+                $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
+                echo '<h4>'.$sujet3.'</h4>';
+                echo '<img src="'.$getImgClean.'">';
+                echo '<li><a href="'.$themes[$sujet3]->channel->item[$i]->link.'">'.($themes[$sujet3]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
             echo '</ul>';
         ?>
