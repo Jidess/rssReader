40620d7be12cb6f3b78c324dd5e553aa4c5705ca
Jidess <102791310+Jidess@users.noreply.github.com>
2022-07-04 15:02:42 +0200

    ajout de la sheet dark

--- views/pages.php
+++ views/pages.php
@@ -8,12 +8,13 @@
     <div class="row">
         <div class="col-12">
         <?php
-            echo '<h4 class="text-center">'.$_GET["theme"].'</h4>';
+            
             echo '<ul class="list-group ">';
+            echo '<li><h4 class="list-group-item text-center bg-secondary">'.$_GET["theme"].'</h4></li>';
             for($i = 0; $i < $radioCookie; $i++){
                 $datetime = date_create($themes[$_GET["theme"]]->channel->item[$i]->pubDate);
                 $date = date_format($datetime, 'd M Y, H\hi');
-                echo '<li class="list-group-item bg-secondary bg-opacity-25"><a href="'.$themes[$_GET["theme"]]->channel->item[$i]->link.'">'.($themes[$_GET["theme"]]->channel->item[$i]->title).'</a> ' . $date . ' ' . $themes[$_GET["theme"]]->channel->item[$i]->description . '</li>';
+                echo '<li class="list-group-item bg-secondary "><a href="'.$themes[$_GET["theme"]]->channel->item[$i]->link.'">'.($themes[$_GET["theme"]]->channel->item[$i]->title).'</a> ' . $date . ' ' . $themes[$_GET["theme"]]->channel->item[$i]->description . '</li>';
             }
             echo '</ul>';
         ?>
