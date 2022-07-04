86566439b8d5ffc9f4e83a1efc9acc0b8356f36b
maxime.rattez <maxime.rattez@gmail.com>
2022-07-04 14:17:16 +0100

    first commit hihihihihihihihihihihi

--- views/home.php
+++ views/home.php
@@ -2,6 +2,7 @@
     <div class="row">
         <div class="col-12 col-md-4">
         <?php
+            echo '<h4>'.$sujet1.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
                 $datetime = date_create($themes[$sujet1]->channel->item[$i]->pubDate);
@@ -10,7 +11,6 @@
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
-                echo '<h4>'.$sujet1.'</h4>';
                 echo '<img src="'.$getImgClean.'">';
                 echo '<li><a href="'.$themes[$sujet1]->channel->item[$i]->link.'">'.($themes[$sujet1]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
@@ -19,6 +19,7 @@
         </div>
         <div class="col-12 col-md-4">
         <?php
+            echo '<h4>'.$sujet2.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
                 $datetime = date_create($themes[$sujet2]->channel->item[$i]->pubDate);
@@ -27,7 +28,6 @@
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
-                echo '<h4>'.$sujet2.'</h4>';
                 echo '<img src="'.$getImgClean.'">';
                 echo '<li><a href="'.$themes[$sujet2]->channel->item[$i]->link.'">'.($themes[$sujet2]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
@@ -36,6 +36,7 @@
         </div>
         <div class="col-12 col-md-4">
         <?php
+            echo '<h4>'.$sujet3.'</h4>';
             echo '<ul>';
             for($i = 0; $i < $radioCookie; $i++){
                 $datetime = date_create($themes[$sujet3]->channel->item[$i]->pubDate);
@@ -44,7 +45,6 @@
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
-                echo '<h4>'.$sujet3.'</h4>';
                 echo '<img src="'.$getImgClean.'">';
                 echo '<li><a href="'.$themes[$sujet3]->channel->item[$i]->link.'">'.($themes[$sujet3]->channel->item[$i]->title).'</a> ('.$date.')</li>';
             }
