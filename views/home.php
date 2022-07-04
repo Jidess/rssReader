2469ce90ff6141c9ba0d13181536dd64d82dae58
Jidess <102791310+Jidess@users.noreply.github.com>
2022-07-04 19:31:57 +0200

    Refonte design homepage

--- views/home.php
+++ views/home.php
@@ -1,53 +1,51 @@
 <div class="container">
     <div class="row ">
-        <div class="col-12 col-md-4 d-flex flex-column align-item-center ">
+    <div class="col-12 col-md-4 d-flex flex-column">
         <?php
             echo '<h4 class="text-center">'.$sujet1.'</h4>';
-            echo '<ul class="list-group">';
+            echo '<div>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet1]->channel->item[$i]->pubDate);
-                $date = date_format($datetime, 'd M Y, H\hi');
+                
                 $description = htmlspecialchars_decode($themes[$sujet1]->channel->item[$i]->description);
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
                 
-                echo '<li class="list-group-item list-group-item-action bg-secondary"><div class="d-flex justify-content-center">'.'<img src="'.$getImgClean.'"> </div><a href="'.$themes[$sujet1]->channel->item[$i]->link.'">'.($themes[$sujet1]->channel->item[$i]->title).'</a> ('.$date.')</li>';
+                echo '<div class="card bg-secondary d-flex flex-column justify-content-center align-items-center cardHome mb-5"><div>'.'<img class="card-img-top imgCard" src="'.$getImgClean.'"> </div> <div class="card-body d-flex flex-column justify-content-center align-items-center"> <p class="card-text">'.($themes[$sujet1]->channel->item[$i]->title).'</p><a href="'.$themes[$sujet1]->channel->item[$i]->link.'"><button class="btn btn-danger"> En savoir plus </button> </a></div></div>';
             }
-            echo '</ul>';
+            echo '</div>';
         ?>
         </div>
         <div class="col-12 col-md-4 d-flex flex-column">
         <?php
             echo '<h4 class="text-center">'.$sujet2.'</h4>';
-            echo '<ul class="list-group">';
+            echo '<div>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet2]->channel->item[$i]->pubDate);
-                $date = date_format($datetime, 'd M Y, H\hi');
+                
                 $description = htmlspecialchars_decode($themes[$sujet2]->channel->item[$i]->description);
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
                 
-                echo '<li class="list-group-item list-group-item-action bg-secondary"><div class="d-flex justify-content-center">'.'<img src="'.$getImgClean.'"> </div><a href="'.$themes[$sujet2]->channel->item[$i]->link.'">'.($themes[$sujet2]->channel->item[$i]->title).'</a> ('.$date.')</li>';
+                echo '<div class="card bg-secondary d-flex flex-column justify-content-center align-items-center cardHome mb-5"><div>'.'<img class="card-img-top imgCard" src="'.$getImgClean.'"> </div> <div class="card-body d-flex flex-column justify-content-center align-items-center"> <p class="card-text">'.($themes[$sujet2]->channel->item[$i]->title).'</p><a href="'.$themes[$sujet2]->channel->item[$i]->link.'"><button class="btn btn-danger"> En savoir plus </button> </a></div></div>';
             }
-            echo '</ul>';
+            echo '</div>';
         ?>
         </div>
-        <div class="col-12 col-md-4 d-flex flex-column ">
+        <div class="col-12 col-md-4 d-flex flex-column">
         <?php
             echo '<h4 class="text-center">'.$sujet3.'</h4>';
-            echo '<ul class="list-group align-item-center  ">';
+            echo '<div>';
             for($i = 0; $i < $radioCookie; $i++){
-                $datetime = date_create($themes[$sujet3]->channel->item[$i]->pubDate);
-                $date = date_format($datetime, 'd M Y, H\hi');
+                
                 $description = htmlspecialchars_decode($themes[$sujet3]->channel->item[$i]->description);
                 preg_match('/src="([^"]+)"/', $description, $matches);
                 $getImgClean = substr($matches[0], 0, -1); // Enlever un quote en trop à la fin
                 $getImgClean = substr($getImgClean, 5); // Nettoyer le lien pour n'avoir que l'url en supprimant src="
-                echo '<li class="list-group-item list-group-item-action bg-secondary"><div class="d-flex justify-content-center">'.'<img src="'.$getImgClean.'"> </div><a href="'.$themes[$sujet3]->channel->item[$i]->link.'">'.($themes[$sujet3]->channel->item[$i]->title).'</a> ('.$date.')</li>';
+                
+                echo '<div class="card bg-secondary d-flex flex-column justify-content-center align-items-center cardHome mb-5"><div>'.'<img class="card-img-top imgCard" src="'.$getImgClean.'"> </div> <div class="card-body d-flex flex-column justify-content-center align-items-center"> <p class="card-text">'.($themes[$sujet3]->channel->item[$i]->title).'</p><a href="'.$themes[$sujet3]->channel->item[$i]->link.'"><button class="btn btn-danger"> En savoir plus </button> </a></div></div>';
             }
-            echo '</ul>';
+            echo '</div>';
         ?>
         </div>
     </div>
